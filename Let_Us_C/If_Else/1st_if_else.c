199986581832d3c195a1bd57fc62bc656379b116
shubha <shubhabanerjeewin8@gmail.com>
2019-10-25 11:29:58 +0530

    if statement

---
+++ Let_Us_C/If_Else/1st_if_else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+    int a;
+
+    printf("Enter any number which is less then 10\n - ");
+    scanf("%d",&a);
+
+    if (a<10)   
+    {
+        printf("The logic is true\n");
+    }
+    // else
+    // {
+    //     printf("Its flse\n");
+    // }
+    
+    
+}
